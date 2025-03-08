7795967271f74db0ec3d1bc3637a105c77ded69c
carlos <camr.personal@gmail.com>
2025-03-08 00:18:48 -0500

    8-verify-email-endpoint - issue #8

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -20,3 +20,24 @@ export const sendVerificationEmail = async (email,verificationToken) =>{
     throw new Error(`Error sending verification email:${error}`)
   }
 }
+
+export const sendWelcomeEmail = async (email,name) =>{
+  const recipient = [{email}]
+
+  try{
+    const response = await client.send({
+      from: sender,
+      to: recipient,
+      template_uuid: process.env.TEMPLATE_UUID,
+      template_variables: {
+        company_info_name: "Auth Company",
+        name: name
+      }
+    })
+
+    console.log("Welcome email send successfully", response)    
+  }catch(error){
+    console.error("Error sending welcome email", error)    
+    throw new Error(`Error sending welcome email: ${error}`)
+  }
+}
